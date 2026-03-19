3ca8b8d3d6c05f420cff248537991342b5a349d4
paramsxngha <param.ssangha@outlook.com>
2026-03-20 12:53:34 +1300

    Version 0.01
    - Start of development

---
+++ LivestockFeedTracker/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace FarmingFeedingApp
+{
+    public class Animal
+    {
+        // Fields
+        private string animalName;
+        private string animalID;
+        private string speciesName;
+        private string foodTypeName;
+        private double[] foodEachDay;
+        private double gramCost;
+
+        // Constructor
+        public Animal(string animalName, string animalID, string speciesName, string foodTypeName, double[] foodEachDay, double gramCost)
+        {
+            this.animalName = animalName;
+            this.animalID = animalID;
+            this.speciesName = speciesName;
+            this.foodTypeName = foodTypeName;
+            this.foodEachDay = foodEachDay;
+            this.gramCost = gramCost;
+        }
+
+        // Getters
+        public string GetAnimalName()
+        {
+            return animalName;
+        }
+        public string GetAnimalID()
+        {
+            return animalID;
+        }
+        public string GetSpeciesName()
+        {
+            return speciesName;
+        }
+        public string GetFoodTypeName()
+        {
+            return foodTypeName;
+        }
+        public double[] GetFoodEachDay()
+        {
+            return foodEachDay;
+        }
+        public double GetGramCost()
+        {
+            return gramCost;
+        }
+    }
+}
---
+++ LivestockFeedTracker/AppManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace FarmingFeedingApp
+{
+    public class AppManager
+    {
+        // Fields
+        static double farmerBudget;
+        static List<Animal> animalList = new List<Animal>();
+
+        // Constant food costs per gram
+        static readonly double GRASS_SILAGE = 0.0003;
+        static readonly double MAIZE_SILAGE = 0.0004;
+        static readonly double GRAIN_MIX = 0.0006;
+        static readonly double PASTURE_GRASS = 0.0002;
+        static readonly double GRASS_HAY = 0.00035;
+        static readonly double GRAIN_SUPPLEMENT = 0.00055;
+        static readonly double SILAGE = 0.0003;
+        static readonly double CORN = 0.00028;
+        static readonly double SOYBEAN_MEAL = 0.00045;
+        static readonly double WHEAT = 0.0003;
+        static readonly double LAYER_FEED = 0.0004;
+        static readonly double OATS = 0.00031;
+
+        // Constructor
+        public AppManager(double budget)
+        {
+            farmerBudget = budget;
+        }
+    }
+}
